2052f4bd37625194af8d6405309fa73636de3dfc
Tanya Mehta <tanyamehta22official@gmail.com>
2024-04-29 17:40:53 +0530

    If else condition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,19 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
-  title = 'Property Binding';
-  name="peter"
-  disable = false;
+  title = 'If-else condition';
+  show= true;
+  
 }
 
