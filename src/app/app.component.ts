4cec6c1d3bf0605a58b66153472d6bd42aef9095
Tanya Mehta <tanyamehta22official@gmail.com>
2024-04-29 16:02:58 +0530

    Basic Rule of style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Counter with angular';
-  count = 0
-  counter(type:string)
-  {
-    type==='add' ? this.count++:this.count--;
-  }
+  title = 'Styling in angular';
+  
 }
 
