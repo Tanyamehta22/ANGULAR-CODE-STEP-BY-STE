b481e366cf07f1e81dcdf7b9ba290447d0b41ad3
Tanya Mehta <tanyamehta22official@gmail.com>
2024-04-30 10:05:41 +0530

    For Loop | *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { NgIf, NgSwitch } from '@angular/common';
+import { NgFor, NgIf, NgSwitch } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
@@ -6,14 +6,20 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf,NgSwitch],
+  imports: [RouterOutlet, NgIf,NgSwitch,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
   title = 'Switch Case';
-  color="green";
+  users = ['Anil', 'Bhasker', 'Sam', 'Peter', 'Burce'];
+  userDetails=[
+    {name:'Anil', email:'anil@test.com', phone:'888'},
+    {name:'Bhaskar', email:'bha@test.com', phone:'888'},
+    {name:'Sam', email:'sam@test.com', phone:'222'},
+    {name:'Peter', email:'peter@test.com', phone:'888'},
+  ];
   
 }
 
