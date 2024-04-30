12de359a5eda551ef6697fa0369e6428883f9e46
Tanya Mehta <tanyamehta22official@gmail.com>
2024-04-30 15:09:37 +0530

    Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { NgFor, NgIf, NgSwitch } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf,NgSwitch,NgFor],
+  imports: [RouterOutlet, NgIf,NgSwitch,NgFor, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
